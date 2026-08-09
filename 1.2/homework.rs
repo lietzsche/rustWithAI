b2778eefbc75d123586cf7348f849e363f37a57c
bion0625 <stxtory@gmail.com>
2026-08-09 21:23:36 +0900

    1-2. 변수

---
+++ 1.2/homework.rs
@@ -0,0 +1,8 @@
+fn main() {
+    let hp = 100;
+    let gold = 0;
+    println!("=== Rust Dungen");
+    println!("HP: {}", hp);
+    println!("Gold: {}", gold);
+    println!("Adventure begins!");
+}
