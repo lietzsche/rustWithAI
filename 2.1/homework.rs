3366484fadbf1f545401f4c2cf67ab009aad1b44
lietzsche <stxtory@gmail.com>
2026-08-13 07:56:22 +0900

    2-1. if와 else 조건문

---
+++ 2.1/homework.rs
@@ -0,0 +1,41 @@
+fn show_player(name: &str, hp: i32, gold: u32) {
+    println!("name: {}", name);
+    println!("hp: {}", hp);
+    println!("gold: {}", gold);
+}
+
+fn show_monster(monster_name: &str, hp: i32) {
+    println!("monster_name: {}", monster_name);
+    println!("hp: {}", hp);
+}
+
+fn attack(name: &str, target_name: &str, damage: i32) {
+    println!("name: {}", name);
+    println!("target_name: {}", target_name);
+    println!("damage: {}", damage);
+}
+
+fn calculate_damage(attack_power: i32, defense: i32) -> i32 {
+    if attack_power > defense {
+        attack_power - defense
+    } else {
+        0
+    }
+}
+
+fn main() {
+    let person_name = "good man";
+    let person_hp = 100;
+    let gold = 30;
+    let monster_name = "bad bird";
+    let monster_hp = 80;
+
+    let mut damage = calculate_damage(35, 12);
+
+    show_player(person_name, person_hp, gold);
+    show_monster(monster_name, monster_hp);
+    attack(monster_name, person_name, damage);
+
+    damage = calculate_damage(10, 15);
+    attack(monster_name, person_name, damage);
+}
